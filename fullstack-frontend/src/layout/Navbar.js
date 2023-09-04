42c34b9bd24d1a5e97c7f7ef07a27bfe0cc265e2
NikSoftelevation <122023966+NikSoftelevation@users.noreply.github.com>
2023-09-04 13:36:21 +0530

    REST API's implemeted in frontend

--- fullstack-frontend/src/layout/Navbar.js
+++ fullstack-frontend/src/layout/Navbar.js
@@ -1,11 +1,12 @@
 import React from "react"
+import { Link } from "react-router-dom"
 
 export default function Navbar() {
     return (
         <div>
             <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#">Full Stack Application</a>
+                    <Link className="navbar-brand" to="/">Full Stack Application</Link>
                     <button
                         className="navbar-toggler"
                         type="button"
@@ -14,9 +15,10 @@ export default function Navbar() {
                         aria-controls="navbarSupportedContent"
                         aria-expanded="false"
                         aria-label="Toggle Navigation"
-                    ><span className="navbar-toggler-icon"></span></button>
+                    >
+                        <span className="navbar-toggler-icon"></span></button>
 
-                    <button className="btn btn-outline-light" >Add User</button>
+                    <Link className="btn btn-outline-light" to="/adduser" >Add User</Link>
                 </div>
             </nav>
         </div>
--- fullstack-frontend/src/pages/Home.js
+++ fullstack-frontend/src/pages/Home.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom';
 
 export default function Home() {
 
@@ -10,14 +11,17 @@ export default function Home() {
         loadUsers();
 
     }, []);
-
     const loadUsers = async () => {
 
         const result = await axios.get("http://localhost:8085/api/users/get/all");
 
         setUsers(result.data);
     }
+    const deleteUser = async (id) => {
 
+        await axios.delete(`http://localhost:8085/api/users/delete/${id}`);
+        loadUsers();
+    }
     return (
         <div className='container'>
             <div className='py-4'>
@@ -29,25 +33,19 @@ export default function Home() {
                             <th scope="col">Username</th>
                             <th scope="col">Email</th>
                             <th scope="col">Action</th>
-
                         </tr>
                     </thead>
-
                     <tbody>
-
                         {users.map((user, index) => (
-
                             <tr>
                                 <th scope="row" key={index}>{index + 1}</th>
                                 <td >{user.name}</td>
                                 <td >{user.username}</td>
                                 <td>{user.email}</td>
                                 <td>
-
-                                    <button className="btn btn-primary mx-2">View</button>
-                                    <button className="btn btn-outline-primary mx-2">Edit </button>
-                                    <button className="btn btn-danger mx-2">Delete</button>
-
+                                    <Link to={`/viewuser/${user.id}`} className="btn btn-primary mx-2">View</Link>
+                                    <Link className="btn btn-outline-primary mx-2" to={`/edituser/${user.id}`}>Edit </Link>
+                                    <Link onClick={() => deleteUser(user.id)} className="btn btn-danger mx-2">Delete</Link>
                                 </td>
                             </tr>
                         ))}
